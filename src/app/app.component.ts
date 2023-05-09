fe432f9076243655775167a7c52b9da01c41ff5a
gustavo <gustavo@stallos.com.br>
2023-05-09 11:16:44 -0300

    Finishing the app using promise and json-server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [HomeComponent],
+    imports: [HomeComponent, RouterModule],
     template: `
     <main>
         <header class="brand-name">
@@ -12,7 +13,8 @@ import { HomeComponent } from './home/home.component';
         </header>
     </main>
     <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
+        <!-- <app-home></app-home> -->
     </section>
   `,
     styleUrls: ['./app.component.css'],
